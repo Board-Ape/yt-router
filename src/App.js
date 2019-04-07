375df3b3d28cfdd931975ec641045173219b4b00
Sam Singer <samuel.singer1@gmail.com>
2019-04-07 08:55:29 -0700

    Using post_id information through props.match.params.post_id

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Posts from './components/Posts';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
+          <Route path="/:post_id" component={Posts} />
         </div>
       </BrowserRouter>
     );
