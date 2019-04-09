79d8bd4af0c810aaed42d7c0eab611a988b60945
Sam Singer <samuel.singer1@gmail.com>
2019-04-09 16:42:30 -0700

    Redux and delete action is set up

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 class Posts extends Component {
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id)
+        this.props.history.push('/')
+    }
+
     render() {
+        console.log(this.props)
         const post = this.props.post ? (
             <div className="post">
                 <h4 className="center">{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={this.handleClick}>DELETE POST</button>
+                </div>
             </div>
         ) : (
             <div className="center">Loading... </div>
@@ -27,4 +36,13 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(Posts);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch({ type: 'DELETE_POST', id: id }) }
+    }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Posts);
