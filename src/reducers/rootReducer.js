feedc1398338e8f289272c0d99ffbec471ee4310
Sam Singer <samuel.singer1@gmail.com>
2019-04-09 16:20:35 -0700

    Connected the redux store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-    posts: []
+    posts: [
+        {id: '1', title: 'Squirtle is the bomb', body: 'Lost in a land far away'},
+        {id: '2', title: 'Charmander is on fire', body: 'Compare this with space'},
+        {id: '3', title: 'Pikachu is quick', body: 'Almost lost my hat there'}
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
